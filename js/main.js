69b3b5e4647cf3256225dce430dae9d7fec584dd
battistaennio <ennio.battista99@gmail.com>
2024-06-19 15:11:51 +0200

    ESERCIZIO TERMINATO

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@
 // Milestone 2:
 // Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è la prima e l’utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l’ultima e viceversa per l’ultima miniatura se l’utente clicca la freccia verso sinistra.
 
+// array oggetti
 const images = [ 
     {   
         image: 'img/01.webp', 
@@ -127,8 +128,3 @@ previous.addEventListener("click",
         }
     }
 );
-
-
-
-
-
